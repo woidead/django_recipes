2564dd95e8bcd7a242658d9b8fef14ac66b2bd90
woidead <abikbest05@gmail.com>
2024-02-24 16:17:32 +0600

    created views and the've connected

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from recipes.views import index, RecipeListView
 
 urlpatterns = [
-    
+    path('', index, name="base"),
+    path('recipes/', RecipeListView, name="recipe_list"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from recipes.models import Recipe
 
-# Create your views here.
+def index(request):
+    """Главная страница"""
+    return render(request, 'recipes/index.html')
+
+class RecipeListView(ListView):
+    """Представление списка рецептов"""
+    model = Recipe
+    template_name = 'recipes/recipe_list.html'
