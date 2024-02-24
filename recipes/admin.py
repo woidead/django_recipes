e0e5d55ab1cb3ce15099815ed30e545d18b5e2d2
woidead <abikbest05@gmail.com>
2024-02-24 15:54:35 +0600

    models connected to admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from recipes.models import Recipe, Ingredient, Comment
 
-# Register your models here.
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Comment)
